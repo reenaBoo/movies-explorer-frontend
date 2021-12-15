1e7f04e70602c38008c5163cb6cf7173ed5226ec
Karina Lyapina <ivettra@yandex.ru>
2021-12-15 20:13:56 +0500

    refactor: update react-router-dom version

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Header from '../Header/Header';
 import Main from '../Main/Main';
 import Movies from '../Movies/Movies';
@@ -11,36 +11,78 @@ import PageNotFound from '../PageNotFound/PageNotFound';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/">
-        <Header />
-        <Main />
-        <Footer />
-      </Route>
-      <Route exact path="/movies">
-        <Header />
-        <Movies />
-        <Footer />
-      </Route>
-      <Route exact path="/saved-movies">
-        <Header />
-        <Movies />
-        <Footer />
-      </Route>
-      <Route exact path="/profile">
-        <Header />
-        <Profile />
-      </Route>
-      <Route exact path="/signup">
-        <Register />
-      </Route>
-      <Route exact path="/signin">
-        <Login />
-      </Route>
-      <Route exact path="/*">
-        <PageNotFound />
-      </Route>
-    </Switch>
+    <Routes>
+      <Route
+        exact
+        path="/"
+        element={
+          <>
+            <Header />
+            <Main />
+            <Footer />
+          </>
+        }
+      />
+      <Route
+        exact
+        path="/movies"
+        element={
+          <>
+            <Header />
+            <Movies />
+            <Footer />
+          </>
+        }
+      />
+      <Route
+        exact
+        path="/saved-movies"
+        element={
+          <>
+            <Header />
+            <Movies />
+            <Footer />
+          </>
+        }
+      />
+      <Route
+        exact
+        path="/profile"
+        element={
+          <>
+            <Header />
+            <Profile />
+          </>
+        }
+      />
+      <Route
+        exact
+        path="/signup"
+        element={
+          <>
+            <Register />
+          </>
+        }
+      />
+      <Route
+        exact
+        path="/signin"
+        element={
+          <>
+            <Login />
+          </>
+        }
+      />
+      <Route
+        exact
+        path="/*"
+        element={
+          <>
+            <PageNotFound />
+          </>
+        }
+      />
+    </Routes>
   );
 }
 
